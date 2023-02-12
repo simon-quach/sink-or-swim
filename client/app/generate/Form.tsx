02695ab2adc34026a3feddf0923004156ba6ef0f
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2023-02-12 01:59:32 -0800

    Post works

--- client/app/generate/Form.tsx
+++ client/app/generate/Form.tsx
@@ -6,7 +6,23 @@ export default function Form() {
   // FUNCTIONS
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    axios.post(''); // Put your server route here
+    const form = e.target as HTMLFormElement;
+    let data = {
+      name: (form[0] as HTMLInputElement).value,
+      age: (form[1] as HTMLInputElement).value,
+      siblings: (form[2] as HTMLInputElement).value,
+      spouses: (form[3] as HTMLInputElement).value,
+      children: (form[4] as HTMLInputElement).value,
+      parents: (form[5] as HTMLInputElement).value,
+      fare: (form[6] as HTMLInputElement).value,
+      passengerClass: (form[7] as HTMLInputElement).value,
+      sex: (form[8] as HTMLInputElement).value,
+      embarkingLocation: (form[9] as HTMLInputElement).value,
+    }
+    console.log(data);
+    axios.post("http://localhost:5000/predict", data).then((res) => {
+      console.log(res);
+    })
   }
 
   return (
@@ -17,12 +33,18 @@ export default function Form() {
       <div className="">Age</div>
       <input type="number" className="bg-[rgba(0,0,0,0.2)] border-[2px] border-[rgba(0,0,0,0)] focus:ring-violet-500 focus:border-violet-500 focus:outline-none text-sm rounded-lg block p-3 text-center"/>
       
-      <div className="">Number of Siblings & Spouses Traveling With</div>
+      <div className="">Number of Siblings On Board</div>
       <input type="number" className="bg-[rgba(0,0,0,0.2)] border-[2px] border-[rgba(0,0,0,0)] focus:ring-violet-500 focus:border-violet-500 focus:outline-none text-sm rounded-lg block p-3 text-center"/>
       
-      <div className="">Number of Children/Parents</div>
+      <div className="">Number of Spouses On Board</div>
       <input type="number" className="bg-[rgba(0,0,0,0.2)] border-[2px] border-[rgba(0,0,0,0)] focus:ring-violet-500 focus:border-violet-500 focus:outline-none text-sm rounded-lg block p-3 text-center"/>
       
+      <div className="">Number of Children On Board</div>
+      <input type="number" className="bg-[rgba(0,0,0,0.2)] border-[2px] border-[rgba(0,0,0,0)] focus:ring-violet-500 focus:border-violet-500 focus:outline-none text-sm rounded-lg block p-3 text-center"/>
+      
+      <div className="">Number of Parents On Board</div>
+      <input type="number" className="bg-[rgba(0,0,0,0.2)] border-[2px] border-[rgba(0,0,0,0)] focus:ring-violet-500 focus:border-violet-500 focus:outline-none text-sm rounded-lg block p-3 text-center"/>
+
       <div className="">Fare Paid</div>
       <input type="number" className="bg-[rgba(0,0,0,0.2)] border-[2px] border-[rgba(0,0,0,0)] focus:ring-violet-500 focus:border-violet-500 focus:outline-none text-sm rounded-lg block p-3 text-center"/>
       
