399dbc081db0b469ca775b225ab2466f3e9f2d88
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2023-02-11 22:09:51 -0800

    openai path set up

--- server/server.py
+++ server/server.py
@@ -2,7 +2,8 @@ from flask import Flask, request, jsonify
 import numpy as np
 from flask_cors import CORS
 from infer import Inferrer
-from chatgpt_wrapper import ChatGPT
+import os
+import openai
 
 bot = ChatGPT()
 response = bot.ask("Hello, world")
@@ -16,6 +17,23 @@ inferrer = Inferrer()
 @app.route("/")
 def home():
     return "Sink or Swim"
+
+@app.route("/generate", methods=["POST"])
+def generate():
+    data = request.get_json()
+    prompt = "Write me an emotional and meaningful story about someone who " + data["survived"] + " on the Titanic with the following information: "
+    prompt += "Name: " + data["name"] + " Number of Siblings Aboard: " + data["siblings"] + " Number of Spouses Aboard: " + data["spouse"] + " Number of Children Aboard: " + data["children"]
+    prompt += " Number of Parents Aboard: " + data["parents"] + " Fare Paid: " + data["fare"] + "Embarked: " + data["embarked"] + " Gender: " + data["gender"] + " Age: " + data["age"] + " Passenger Class: " + data["class"]
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=1000,
+        top_p=1,
+        frequency_penalty=0.5,
+        presence_penalty=0.5)
+
+    return jsonify({"response": response["choices"][0]["text"]})
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000)
