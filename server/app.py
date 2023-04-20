86f44d649673a606b591ce207c805a75ce10af2f
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2023-04-19 19:34:40 -0700

    New updates to server

--- server/app.py
+++ server/app.py
@@ -31,7 +31,8 @@ limiter = Limiter(
 @app.route("/")
 @limiter.limit("5/hour", methods=["GET"])
 def home():
-    return "Sink or Swim"
+    html_code = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Sink or Swim</title><link rel="stylesheet" href="styles.css"></head><body><header><h1>Sink or Swim</h1><p>A Titanic prediction and storytelling web application.</p></header><section><h2>About the App</h2><p>Sink or Swim is a web application that uses machine learning to make predictions and generate emotional, meaningful stories about Titanic passengers. The app utilizes the OpenAI GPT-3.5-turbo model and the Flask web framework to provide users with a seamless and engaging experience.</p></section><section><h2>Features</h2><ul><li>Predict Titanic passengers\' survival chances based on their personal information.</li><li>Generate personalized stories about Titanic passengers who survived the tragedy.</li></ul></section><section><h2>How to Use</h2><ol><li>Visit the "/predict" endpoint to make predictions using the POST method with a JSON payload containing relevant passenger information.</li><li>Visit the "/generate" endpoint to generate stories using the POST method with a JSON payload containing relevant passenger information and a survival scenario.</li></ol></section><footer><p>&copy; 2023 Sink or Swim. All rights reserved.</p></footer></body></html>'
+    return html_code
 
 
 @app.route("/predict", methods=["POST"])
